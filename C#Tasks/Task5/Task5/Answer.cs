393104cdc1a1d4ec2e3ab4064bfc1ca54735f8a7
menna-magdy0-0 <mennamagdy4291@gmail.com>
2025-01-09 12:34:45 +0200

    Task5 C#

---
+++ C#Tasks/Task5/Task5/Answer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5
+{
+    internal class Answer
+    {
+        int num;
+        string choicetext;
+        public Answer()
+        {
+            Num = 1;
+            Choicetext = "Object-Oriented Programming";
+        }
+        public Answer(int _num)
+        {
+            Num = _num;
+            Choicetext = "Object-Oriented Programming";
+        }
+        public Answer(string _choicetext)
+        {
+            Num = 1;
+            Choicetext = _choicetext;
+        }
+        public Answer(int _num, string _choicetext)
+        {
+            Num = _num;
+            Choicetext = _choicetext;
+        }
+
+        public int Num { get => num; set => num = value; }
+        public string Choicetext { get => choicetext; set => choicetext = value; }
+
+        public override string ToString()
+        {
+            return $"{Num}::{Choicetext}";
+        }
+    }
+}
---
+++ C#Tasks/Task5/Task5/Question.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5
+{
+    internal class Question
+    {
+        string questionbody;
+        int mark;
+        public Question()
+        {
+            Questionbody = "What does OOP stand for?";
+            Mark = 10;
+        }
+        public Question(string _questionbody)
+        {
+            Questionbody = _questionbody;
+            Mark = 10;
+        }
+        public Question(int _mark)
+        {
+            Questionbody = "What does OOP stand for?";
+            Mark = _mark;
+        }
+        public Question(string _questionbody, int _mark)
+        {
+            Questionbody = _questionbody;
+            Mark = _mark;
+        }
+
+        public string Questionbody { get => questionbody; set => questionbody = value; }
+        public int Mark { get => mark; set => mark = value; }
+
+        public override string ToString()
+        {
+            return $"{Questionbody}::{Mark}";
+        }
+    }
+}
